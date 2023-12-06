2b9a0a17961c8948589ab2f8529acaf72d03decb
chrispwd <chrispwd@protonmail.com>
2023-12-06 14:14:43 -0700

    update/solution day 1, part 2

--- day1/main.go
+++ day1/main.go
@@ -14,28 +14,79 @@ func main() {
 	if err != nil {
 		log.Fatalf("readLines: %s", err)
 	}
-	fmt.Println(calibrateDocument(document))
+	fmt.Println(asciiArt())
+	fmt.Println("Calibration Reading:", calibrateDocument(document))
 }
 
-// TODO :: Finish Part 2
 func calibrateDocument(doc []string) int {
 	var acc int = 0
 	_ = acc
 	for i, line := range doc {
 		_ = i
-		re := regexp.MustCompile(`([0-9]){1}`)
-		nums := re.FindAllString(line, -1)
-		num, err := strconv.Atoi(fmt.Sprintf("%v%v", nums[0], nums[len(nums)-1]))
+		// DEBUG
+		// fmt.Println("BEGIN:", line)
+		strNums := unspell(line)
+		intNums, err := strconv.Atoi(fmt.Sprintf("%v%v", strNums[0], strNums[len(strNums)-1]))
+		// DEBUG
+		// fmt.Println("END:", intNums)
 		if err != nil {
 			continue
 		}
-		acc += num
+		acc += intNums
 	}
 	return acc
 }
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
+func unspell(line string) []string {
+	var raw []string
+	re := regexp.MustCompile(`([0-9]|one|two|three|four|five|six|seven|eight|nine){1}`)
+	for true {
+		var match string = re.FindString(line)
+		var loc []int = re.FindStringIndex(line)
+		// DEBUG
+		// fmt.Println("match:", match)
+		if loc == nil {
+			break
+		}
+		switch match {
+		case "one":
+			raw = append(raw, "1")
+			line = line[loc[1]-1:]
+		case "two":
+			raw = append(raw, "2")
+			line = line[loc[1]-1:]
+		case "three":
+			raw = append(raw, "3")
+			line = line[loc[1]-1:]
+		case "four":
+			raw = append(raw, "4")
+			line = line[loc[1]:]
+		case "five":
+			raw = append(raw, "5")
+			line = line[loc[1]-1:]
+		case "six":
+			raw = append(raw, "6")
+			line = line[loc[1]:]
+		case "seven":
+			raw = append(raw, "7")
+			line = line[loc[1]-1:]
+		case "eight":
+			raw = append(raw, "8")
+			line = line[loc[1]-1:]
+		case "nine":
+			raw = append(raw, "9")
+			line = line[loc[1]-1:]
+		default:
+			raw = append(raw, match)
+			line = line[loc[1]:]
+		}
+		// DEBUG
+		// fmt.Println("after switch:", line)
+		// fmt.Println("raw:", raw)
+	}
+	return raw
+}
+
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -49,4 +100,37 @@ func readLines(path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, scanner.Err()
+}
+
+func asciiArt() string {
+	elf := `                           '-.
+                         ,'   |
+                       ,'     |
+                     ,'       :
+                   ,<\        \
+                 ,' //         .
+               ,' ,'/          :
+             ,'  /,'           |
+            /._,'/             |
+           /.__,'              |
+          /                    |
+       .-'                     |
+       '.                      |
+         |'-._          ,      |
+         ; -. '-.     ,'|      :
+        / |@)\  |   ,', ;      '
+       /        |  : /| |     /
+      /         |  |: | |     :
+    ,'          |  |'-' ;     |
+   |   .      ,''._|   /-._   '
+   '-.\ )   ,'       -'    -,-'
+      |    :                :
+      ;._                   |
+      \__'.         )       |
+      /_         ,-'        |
+     ,'       ,-'           |
+    (     _,-'\             |
+     '._,'     :            | SSt
+               '`
+	return elf
 }
