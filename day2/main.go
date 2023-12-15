838fa19b02138850a054745f0a55bf261ae7648d
chrispwd <chrispwd@protonmail.com>
2023-12-15 09:58:45 -0700

    wip capture game IDs

--- day2/main.go
+++ day2/main.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-	// "regexp"
+	"regexp"
+	"strconv"
 )
 
-const R int = 12
-const G int = 13
-const B int = 14
+var validation = map[string]int{
+	"R": 12,
+	"G": 13,
+	"B": 14,
+}
+
+// const R int = 12
+// const G int = 13
+// const B int = 14
 
 func main() {
 	games, err := readLines("input.txt")
@@ -18,24 +25,27 @@ func main() {
 		log.Fatalf("readLines: %s", err)
 	}
 	fmt.Println(asciiArt())
-	fmt.Println("Sum of Valid Game IDs:", gameSum(games))
+	fmt.Println("Sum of Valid Game IDs:", gameSum(games, validation))
 }
 
 // Part 1
-func gameSum(games []string) int {
+func gameSum(games []string, validation map[string]int) int {
 	// TODO :: you are here
+	gameAcc := 0
 	for _, line := range games {
 
 		// capture gameID (quick regex?)
+		re := regexp.MustCompile(`[0-9]+`)
+		var gId, _ = strconv.Atoi(re.FindString(line))
 		// Split the sets by semicolon
 		// Split the colors
 		// Compare if colcount > constants, skip line
 
 		// DEBUG
+		gameAcc += gId
 		fmt.Println(line)
-
 	}
-	return 0
+	return gameAcc
 }
 
 func readLines(path string) ([]string, error) {
---
+++ go.mod
@@ -0,0 +1,9 @@
+module aoc2023
+
+go 1.21.4
+
+require (
+	github.com/magefile/mage v1.14.0 // indirect
+	github.com/tetratelabs/wazero v1.5.0 // indirect
+	github.com/wasilibs/go-re2 v1.4.1 // indirect
+)
